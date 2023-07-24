f9f13f5a42af44a4b873dacf73c33ebcdd1b5fd5
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-07-24 12:48:22 +0200

    Update dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,21 +1,17 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-const countParagraph = $('#count');
-const button = $('<button></button>').text('Click here to get started');
-
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append(button);
-$('body').append(countParagraph);
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
+let conunt = 0;
 
 function updateCounter() {
   count++;
-  countParagraph.text(`${count} clicks on the button`);
-}
+  $("#count").html(`${count} clicks on the button`);
+};
 
-button.on('click', _.debounce(updateCounter, 1000));
-
+$('button').on('click', _.debounce(updateCounter, 500));
