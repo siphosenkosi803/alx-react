fe04e3668bbcf73d9eacbfe1ef49ffcbdf210295
siphosenkosi803 <sibusisoradebe943yahoo.com>
2023-09-04 18:07:43 +0000

    1. Create a small store, Test MapStateToProps

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
@@ -1,76 +1,25 @@
+import { shallow, mount } from "enzyme";
 import React from "react";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Login from "../Login/Login";
-import CourseList from "../CourseList/CourseList";
-import Notifications from "../Notifications/Notifications";
-import "./App.css";
-import PropTypes from "prop-types";
-import { getLatestNotification } from "../utils/utils";
+import App, { listNotificationsInitialState, mapStateToProps } from "./App";
+import { StyleSheetTestUtils } from "aphrodite";
+import AppContext, { user, logOut } from "./AppContext";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+import { fromJS } from "immutable";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import uiReducer, { initialState } from "../reducers/uiReducer";
 
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-  }
+const store = createStore(uiReducer, initialState);
 
-  listCourses = [
-    { id: 1, name: "ES6", credit: 60 },
-    { id: 2, name: "Webpack", credit: 20 },
-    { id: 3, name: "React", credit: 40 },
-  ];
+describe("<App />", () => {
+  it("mapStateToProps returns the right object from user Login", () => {
+    let state = fromJS({
+      isUserLoggedIn: true,
+    });
 
-  listNotifications = [
-    { id: 1, type: "default", value: "New course available" },
-    { id: 2, type: "urgent", value: "New resume available" },
-    { id: 3, type: "urgent", html: getLatestNotification() },
-  ];
+    const result = mapStateToProps(state);
 
-  handleKeyPress(e) {
-    if (e.ctrlKey && e.key === "h") {
-      alert("Logging you out");
-      this.props.logOut();
-    }
-  }
-
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyPress);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyPress);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className="App">
-          <div className="heading-section">
-            <Notifications listNotifications={this.listNotifications} />
-            <Header />
-          </div>
-          {this.props.isLoggedIn ? (
-            <CourseList listCourses={this.listCourses} />
-          ) : (
-            <Login />
-          )}
-          <Footer />
-        </div>
-      </React.Fragment>
-    );
-  }
-}
-
-App.defaultProps = {
-  isLoggedIn: false,
-  logOut: () => {},
-};
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
-};
-
-export default App;
+    expect(result).toEqual({ isLoggedIn: true });
+  });
+});
 
